f84c34d5b790a6db6944bfab1331e05b17af00c2
mukul0512-tiwari <mukul512tiwari@gmail.com>
2025-08-26 15:25:04 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+#perform basic mathematics
+a=input( 'enter the first number:')
+b=input( 'enter the second number:')
+
+a=float(a)
+b=float(b)
+
+print("Addition: ",int(a+b))
+print("Subtraction: ",int(a-b))
+print("Multiplication: ",int(a*b))
+print("Division: ",float(a/b))
